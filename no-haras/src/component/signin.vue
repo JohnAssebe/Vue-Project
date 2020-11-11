b8a3f239b8ab16b2259c10793c81288719d2ae38
Yohannes Assebe <39563000+JohnAssebe@users.noreply.github.com>
2020-11-12 01:04:01 +0300

    signin_with_firebase_check

--- no-haras/src/component/signin.vue
+++ no-haras/src/component/signin.vue
@@ -5,6 +5,10 @@
 <label>User Name:</label><input type='text' v-model=login.username><br>
 <label>Password</label><input type='password' v-model=login.password><br>
 <button v-on:click.prevent='submit'>Login</button>
+<div>
+<label v-show="right=='wrong'" style="color:red"> Username Or Password Error</label>
+<label v-show="right=='right'" style="color:green">Succesfull</label>
+</div>
 </form>
 </div>
 </template>
@@ -16,13 +20,34 @@ export default{
             login:{
                 username:'',
                 password:'',
-            }
+            },
+            user:[],
+            right:''
 
     }
     },
     methods:{
         submit:function(){
-            console.log(this.login);
+            this.$http.get('https://no-bully-1b47c.firebaseio.com/register.json').then(function(data){
+                return data.json();
+
+            }).then(function(data){
+                for(let key in data){
+                    this.user.push(data[key].username)
+                  if(this.user.includes(this.login.username)){
+                     if(data[key].password==this.login.password){
+                         this.right="right"
+                     }
+                     else{
+                         this.right="wrong"
+                     }
+
+                  } 
+                  else{
+                      this.right='wrong'
+                  }
+                }
+            })
         }
     },
     components:{
